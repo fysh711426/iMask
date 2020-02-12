ce1853d30c55b810c71a9495f70d86a29a968e53
fysh711426 <fysh711426@hotmail.com>
2020-02-12 10:02:54 +0800

    add iMaskMap popup

--- iMask/wwwroot/main.js
+++ iMask/wwwroot/main.js
@@ -20,16 +20,53 @@ var redIcon = new L.Icon({
 });
 L.marker([latitude, longitude], { icon: redIcon }).addTo(map);
 
-var markers = new L.MarkerClusterGroup().addTo(map);
-
-var imageIcon = new L.Icon({
-    iconUrl: 'https://ithelp.ithome.com.tw/upload/images/20200210/20106865an2gNAyvHQ.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-    popupAnchor: [1, -40],
-    shadowSize: [40, 40]
+var image0Icon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/fysh711426/fysh711426.github.io/master/iMaskMap/image/iMask_back_0.png',
+    iconSize: [86, 86],
+    iconAnchor: [43, 86],
+    popupAnchor: [1, -86]
 });
+
+var image1Icon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/fysh711426/fysh711426.github.io/master/iMaskMap/image/iMask_back_1.png',
+    iconSize: [86, 86],
+    iconAnchor: [43, 86],
+    popupAnchor: [1, -86]
+});
+
+var image2Icon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/fysh711426/fysh711426.github.io/master/iMaskMap/image/iMask_back_2.png',
+    iconSize: [86, 86],
+    iconAnchor: [43, 86],
+    popupAnchor: [1, -86]
+});
+
+var image3Icon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/fysh711426/fysh711426.github.io/master/iMaskMap/image/iMask_back_3.png',
+    iconSize: [86, 86],
+    iconAnchor: [43, 86],
+    popupAnchor: [1, -86]
+});
+
+var imageClass = ["image0-icon", "image1-icon", "image2-icon", "image3-icon"];
+
+var markers = new L.MarkerClusterGroup({
+    iconCreateFunction: function (cluster) {
+        var list = cluster.getAllChildMarkers();
+        var level = 0;
+        for (var i = 0; i < list.length; i++) {
+            if (level < 3 && list[i].options.icon.options.iconUrl === image3Icon.options.iconUrl)
+                level = 3;
+            else if (level < 2 && list[i].options.icon.options.iconUrl === image2Icon.options.iconUrl)
+                level = 2;
+            else if (level < 1 && list[i].options.icon.options.iconUrl === image1Icon.options.iconUrl)
+                level = 1;
+        }
+        return L.divIcon({ html: '<div><span>' + cluster.getChildCount() + '</span></div>', className: "icon-cluster " + imageClass[level], iconSize: [50, 50] });
+    },
+    removeOutsideVisibleBounds: true,
+    animate: true
+}).addTo(map);
 
 var xhr = new XMLHttpRequest();
 xhr.open('get', 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json');
@@ -37,13 +74,57 @@ xhr.send();
 xhr.onload = function () {
     var data = JSON.parse(xhr.responseText).features;
     for (var i = 0; i < data.length; i++) {
+
+        if (data[i].geometry.coordinates[1] >= 30 ||
+            data[i].geometry.coordinates[1] <= 0 ||
+            isNaN(data[i].geometry.coordinates[1]))
+            continue;
+
+        var imageIcon;
+        if (data[i].properties.mask_adult >= 50)
+            imageIcon = image3Icon;
+        else if (data[i].properties.mask_adult > 20)
+            imageIcon = image2Icon;
+        else if (data[i].properties.mask_adult > 0)
+            imageIcon = image1Icon;
+        else
+            imageIcon = image0Icon;
+
         var mark = L.marker([
             data[i].geometry.coordinates[1],
             data[i].geometry.coordinates[0]
-        ], {
-            icon: imageIcon
-        }).bindPopup(data[i].properties.name);
+        ], { icon: imageIcon }
+        ).bindPopup(
+            '<p class="popup-name">' + data[i].properties.name + '<p/>' +
+            '<p class="popup-phone">[電話] ' + data[i].properties.phone + '<p/>' +
+            '<p class="popup-mask">[口罩] 成人: ' + data[i].properties.mask_adult + '、兒童: ' + data[i].properties.mask_child + '<p/>' +
+            '<p class="popup-address">[地址] ' + data[i].properties.address + '<p/>');
         markers.addLayer(mark);
     }
     map.addLayer(markers);
 };
+
+//storeIcon = [
+//    L.icon({ iconUrl: "images/sold-out.svg", iconSize: [48, 48], iconAnchor: [24, 48], popupAnchor: [0, -48] }),
+//    L.icon({ iconUrl: "images/emergency.svg", iconSize: [48, 48], iconAnchor: [24, 48], popupAnchor: [0, -48] }),
+//    L.icon({ iconUrl: "images/warning.svg", iconSize: [48, 48], iconAnchor: [24, 48], popupAnchor: [0, -48] }),
+//    L.icon({ iconUrl: "images/sufficient.svg", iconSize: [48, 48], iconAnchor: [24, 48], popupAnchor: [0, -48] })
+//],
+//    storeClass = ["sold-out", "emergency", "warning", "sufficient"],
+//    purchase = { day: ["日", "一", "二", "三", "四", "五", "六"], parity: ["不限", "奇數", "偶數", "奇數", "偶數", "奇數", "偶數"] },
+//    xhr = new XMLHttpRequest(),
+//    storeMarkers = L.markerClusterGroup({
+//        iconCreateFunction: function (cluster) {
+//            let list = cluster.getAllChildMarkers(),
+//                order = 0;
+//            for (let i = 0; i < list.length; i++) {
+//                order = order < 3 && list[i].options.icon.options.iconUrl === storeIcon[3].options.iconUrl ? 3 :
+//                    order < 2 && list[i].options.icon.options.iconUrl === storeIcon[2].options.iconUrl ? 2 :
+//                        order < 1 && list[i].options.icon.options.iconUrl === storeIcon[1].options.iconUrl ? 1 :
+//                            list[i].options.icon.options.iconUrl === storeIcon[0] ? 0 : order;
+//            }
+//            return L.divIcon({ html: cluster.getChildCount(), className: "icon-cluster " + storeClass[order], iconSize: [72, 30] });
+//        },
+//        removeOutsideVisibleBounds: true,
+//        animate: true
+//    }),
